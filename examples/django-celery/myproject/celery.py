9683b7911d00c1afc3dc2d9d3957834a5a40ff71
Alex (slothy) <slothyrulez@gmail.com>
2025-11-18 16:28:13 +0100

    feat (examples): follow new strategy

--- examples/django-celery/myproject/celery.py
+++ examples/django-celery/myproject/celery.py
@@ -1,7 +1,7 @@
 import os
 
 from celery import Celery
-from celery_redis_statedb import install_redis_statedb, install_redis_statedb_option
+from celery_redis_statedb import install_redis_statedb
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
@@ -9,11 +9,8 @@ app = Celery("myproject")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Install Redis StateDB to replace default file-based persistence
+# Install Redis StateDB (adds CLI option and registers bootstep)
 install_redis_statedb(app)
-
-# Add --redis-statedb CLI option
-install_redis_statedb_option(app)
 
 app.autodiscover_tasks()
 
